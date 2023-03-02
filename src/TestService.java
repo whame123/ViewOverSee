f81cbe9a32c2d90279f0fac0c63364296592b03f
於利艳 <1078419389@qq.com>
2023-03-03 00:47:17 +0800

    第二次提交

---
+++ src/TestService.java
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+public class TestService {
+
+    public static final String LIKE = "LIKE";
+
+    public static void main(String[] args){
+        System.out.println(LIKE);
+    }
+
+}
